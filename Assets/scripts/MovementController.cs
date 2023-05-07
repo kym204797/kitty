c309ec6c833de36945516615c303fb24be1ea52f
anothernickguy <132588165+anothernickguy@users.noreply.github.com>
2023-05-06 22:44:28 -0400

    avance 1 Nicolas Guzman
    bitácora progreso 1:
    empecé con el añadido del menú de inicio algo simple pero que funciona luego me decidí a añadir las animaciones al jugador y debo decir que me costo pero lo logre al moverse el personaje corre y salta

--- Assets/scripts/MovementController.cs
+++ Assets/scripts/MovementController.cs
@@ -8,11 +8,13 @@ public class MovementController : MonoBehaviour
     public float jumpForce = 5f; // fuerza del salto del personaje
 
     private Rigidbody2D rb;
+    private Animator animator; // referencia al componente Animator
     private bool isGrounded = true; // verifica si el personaje está en el suelo
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // obtenemos la referencia al Rigidbody2D del personaje
+        animator = GetComponent<Animator>(); // obtenemos la referencia al Animator del personaje
     }
 
     void Update()
@@ -22,10 +24,14 @@ public class MovementController : MonoBehaviour
         // actualizamos la posición del transform del personaje
         transform.position += Vector3.right * horizontalMovement * speed * Time.deltaTime;
 
+        // establecer el valor del parámetro isRunning en función de si el personaje está moviéndose o no
+        animator.SetBool("isRunning", horizontalMovement != 0);
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            animator.SetBool("isGrounded", false); // establecer el valor del parámetro isGrounded en false
         }
     }
 
@@ -34,6 +40,16 @@ public class MovementController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            animator.SetBool("isGrounded", true); // establecer el valor del parámetro isGrounded en true
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            animator.SetBool("isGrounded", false); // establecer el valor del parámetro isGrounded en false
         }
     }
 }
